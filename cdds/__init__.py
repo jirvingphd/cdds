e1376ea971a5ed263d186c86d7252f21accc5e9f
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2022-11-18 17:31:59 -0500

    Fixed namespace imports

--- cdds/__init__.py
+++ cdds/__init__.py
@@ -4,11 +4,12 @@ __author__ = """James Irving"""
 __email__ = 'james.irving.phd@gmail.com'
 __version__ = '1.0.1'
 
-# from cdds import datasets
-# # from cdds.inspect import check_package_versions
+from . import datasets
+from .inspect import check_package_versions
 # from cdds import lp_functions as lp
-# from cdds import inspect
-# from cdds import filetools
-# from cdds import utils
+from . import lp_functions
+from . import inspect
+from . import filetools
+from . import utils
 
-from cdds.inspect import ihelp 
+from .inspect import ihelp 

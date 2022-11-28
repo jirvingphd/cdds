cb4314eadef1bf832b636ff02d609a32ac800bc6
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2022-11-28 14:39:22 -0500

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/cdds',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )

e77bfa31f0c113dd4624fffce248c900909445a5
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2022-11-28 14:52:04 -0500

    Bump version: 1.0.4 → 1.0.5

--- cdds/__init__.py
+++ cdds/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """James Irving"""
 __email__ = 'james.irving.phd@gmail.com'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 from . import datasets
 # from .inspect import check_package_versions
